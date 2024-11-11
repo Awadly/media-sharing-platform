fb57158ca06c1b73ad3576271cab3aa7a742eccc
Awadly <abdelrahmanalawadly@gmail.com>
2024-11-11 07:57:21 +0200

    Allow CORS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import mediaRoutes from "./routes/mediaRoutes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/api/media", mediaRoutes);
 
