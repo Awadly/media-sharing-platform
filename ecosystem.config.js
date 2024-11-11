e98c056a2746b9d68ae9b2d9180d1ff6bd4106ee
Awadly <abdelrahmanalawadly@gmail.com>
2024-11-11 06:46:42 +0200

    change interpreter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "media-sharing-platform",
       script: "src/server.ts",
-      interpreter: "./node_modules/.bin/ts-node",
+      interpreter: "ts-node",
       watch: true,
       env: {
         NODE_ENV: "production",
